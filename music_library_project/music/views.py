a3fb19396feb708c9d52fccf547619e328c6ebb2
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-14 14:52:29 -0500

    updated Return status codes in the SongDetail class view method in the song.views.py as well as added comments to help assist in comprehention of what is going on.

--- music_library_project/music/views.py
+++ music_library_project/music/views.py
@@ -23,26 +23,30 @@ class SongList(APIView):
 
 class SongDetail(APIView):
 
-    def get_object(self, pk):
+    def get_object(self, pk):  # selecting a song object by the pk id
         try:
-            return Song.objects.get(pk=pk)
-        except Song.DoesNotExist:
-            raise Http404
+            return Song.objects.get(pk=pk)  # here we returning the song by the id
+        except Song.DoesNotExist:  # if the exception 'song.does.not.exist' occurs, ...
+            raise Http404  # then we respond with an HTTP404 response
+        # TODO : check on what raise Http404 does exactly. get clarification
 
-    def get(self, request, pk):
-        song = self.get_object(pk)
-        serializer = SongSerializer(song)
-        return Response(serializer.data)
+    def get(self, request, pk):  # 'get' is getting something fom the database.. in this case a single song
+        song = self.get_object(pk)  # here we are selecting a song by id, and assigning it to the 'song' variable
+        serializer = SongSerializer(song)  # here we are serializing/reformatting the song variable
+        return Response(serializer.data, status=status.HTTP_200_OK)  # here we are returning the selected song, in the serialized/updated format
+    # get song by id functionality
 
-    def put(self, request, pk):
-        song = self.get_object(pk)
-        serializer = SongSerializer(song, data=request.data)
-        if serializer.is_valid():
+    def put(self, request, pk):  # 'put' syntax == update functionality
+        song = self.get_object(pk)  # here we are getting the song by id using the get_object function
+        serializer = SongSerializer(song, data=request.data)  # here we are reformatting the song using the serializer
+        if serializer.is_valid():  # == if the data in the request matches the model ...
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)  # save the selected song with the new data from the request
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # otherwise give 404 error message
+    # update song by id functionality
 
-    def delete(self, request, pk):
-        song = self.get_object(pk)
-        song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, pk):  # delete functionality
+        song = self.get_object(pk)  # here we are getting the song by id using the get_object function above
+        song.delete()  # deleting the song that has the selected pk id
+        return Response(status=status.HTTP_200_OK)  # relaying status info that the the deletion was a success
+    # delete song by id functionality
